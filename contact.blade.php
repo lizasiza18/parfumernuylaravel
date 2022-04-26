ca832e5ad2d51c979f07e904f76d383f17a337b7
lizasiza18 <104332420+lizasiza18@users.noreply.github.com>
2022-04-26 16:51:57 +0800

    Add files via upload

---
+++ contact.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CHARMS</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<font-face="Roboto">
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Начальная загрузка"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/welcome" class="nav-link px-2 link-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Главная</font></font></a></li>
+          <li><a href="/about" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">О нас</font></font></a></li>
+          
+          <li><a href="/contact" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Контакты</font></font></a></li>
+          <li><a href="/table1" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ассортимент</font></font></a></li>
+          <li><a href="/table" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Заказать</font></font></a></li>
+        </ul>
+        <style type="text/css">
+  .border-bottom   {
+    background: Thistle; 
+    color: black; 
+   }
+  
+  </style>
+      </div>
+    </div>
+  </header>
+</div>
+</head>
+<body style="font-family:Courier New, monospace, sans-serif">
+    <div class="site-contact">
+        <div class="container">
+            <center><h1>-Как с нами связаться:-</h1></center>
+
+            <h5>
+                <br><li>
+                    665805, Иркутская область, город Ангарск, 22 мкрорайон, дом 42
+
+                    <br>
+                <li>
+                    Телефон горячей линии: <u>(3955)71-61-55</u>
+
+                    <br>
+                <li>
+                    Телефон для информации о заказе: <u>(3955) 12-70-71</u>
+
+                    <br>
+                <li>Адрес электронной почты: <u>charms@angarsk.ru</u>
+            </h5>
+            <center><img src="https://sun9-4.userapi.com/impg/Yo4fVxKnVsiQrjSn6y8gDoTXv3a7wJorgVBTKQ/Fu3wu2MZvU0.jpg?size=427x351&quality=96&sign=e2cedde9d729d00d08178026610340c3&type=album" width="350" height="300"></center>
+        </div>
+
+    </div>
---
+++ table1.blade.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CHARMS</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<font-face="Roboto">
+<header class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Начальная загрузка"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="/welcome" class="nav-link px-2 link-secondary"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Главная</font></font></a></li>
+          <li><a href="/about" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">О нас</font></font></a></li>
+          <li><a href="/contact" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Контакты</font></font></a></li>
+          <li><a href="/table1" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Ассортимент</font></font></a></li>
+          <li><a href="/table" class="nav-link px-2 link-dark"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Заказать</font></font></a></li>
+        </ul>
+        <style type="text/css">
+  .border-bottom   {
+    background: Thistle; 
+    color: black; 
+   }
+  
+  </style>
+      </div>
+    </div>
+  </header>
+</div>
+
+</head>
+<body style="font-family:Courier New, monospace, sans-serif">
+
+<h1>Форма для добавления товара</h1>
+<form action= "" method = "POST">
+@csrf
+<input type ="text" name ="Название" id="Название" placeholder = "Введите название" class ="form-control"><br>
+<input type ="text" name ="Цена" id="Цена" placeholder = "Введите цену" class ="form-control"><br>
+<input type ="text" name ="Объем" id="Объем" placeholder = "Введите объем" class ="form-control"><br>
+<input type ="text" name ="Производитель" id="Производитель" placeholder = "Введите производителя" class ="form-control"><br>
+<input type ="text" name ="Страна" id="Страна" placeholder = "Введите страну" class ="form-control"><br>
+<input type ="text" name ="Артикул" id="Артикул" placeholder = "Введите артикул" class ="form-control"><br>
+<input type ="date" name ="Дата_производства"id="Дата_производства" placeholder = "Введите дату производства" class ="form-control"><br>
+<input type ="text" name ="Количество" id="Количество" placeholder = "Введите количество" class ="form-control"><br>
+
+<button type = "submit" class ="btn btn-success">Добавить</button>
+</form>
+
+<h1>Ассортимент</h1>
+<style type="text/css">
+   TABLE {
+    background: rgb(204, 159, 218); 
+    border: 2px double rgb(70, 31, 78); 
+   }
+   TD, TH {
+    padding: 5px; 
+    border: 1px solid rgb(79, 56, 82);
+   }
+  </style>
+<table border = 2>
+<tr>
+<th>#</th>
+<th>Название</th>
+<th>Цена</th>
+<th>Объем</th>
+<th>Производитель</th>
+<th>Страна</th>
+<th>Артикул</th>
+<th>Дата_производства</th>
+<th>Количество</th>
+
+
+</tr>
+
+@foreach($posts as $post)
+<tr>
+<td>{{$post->id}}</td>
+<td>{{$post->Название}}</td>
+<td>{{$post->Цена}}</td>
+<td>{{$post->Объем}}</td>
+<td>{{$post->Производитель}}</td>
+<td>{{$post->Страна}}</td>
+<td>{{$post->Артикул}}</td>
+<td>{{$post->Дата_производства}}</td>
+<td>{{$post->Количество}}</td>
+
+<td><a href='delete/{{$post->id}}'>Удалить</a></td></th>
+</tr>
+@endforeach
+</table>
